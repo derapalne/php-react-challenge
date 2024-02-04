cafffb1d192f0f72d8546c2d39e710aaafab4cf6
dera <derapalne@gmail.com>
2024-02-04 18:38:32 -0300

    Authentication needed for protected routes

--- php-backend/routes/web.php
+++ php-backend/routes/web.php
@@ -21,3 +21,5 @@ Route::get('/', function () {
 
 Route::apiResource('products', ProductController::class)->only(['show', 'index']);
 Route::apiResource('products', CompanyController::class)->only(['show', 'index']);
+Route::middleware('auth:sanctum')->apiResource('products', ProductController::class)->except(['show', 'index']);
+Route::middleware('auth:sanctum')->apiResource('products', CompanyController::class)->except(['show', 'index']);
