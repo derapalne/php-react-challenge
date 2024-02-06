3a1f52934b11c8f6c5ea14f63a8b2a9ca1503573
dera <derapalne@gmail.com>
2024-02-06 00:27:49 -0300

    Add Product Form Pt.1

---
+++ react-frontend/src/app/add-product/page.tsx
@@ -0,0 +1,14 @@
+import AddProductForm from "@/components/AddProductForm";
+import Header from "@/components/Header";
+import LoginForm from "@/components/LoginForm";
+
+export default function Login() {
+    return (
+        <>
+            <Header />
+            <main className="flex min-h-screen w-9/12 bg-slate-100 text-slate-950 mx-auto flex-col items-center pb-12">
+                <AddProductForm />
+            </main>
+        </>
+    );
+}
